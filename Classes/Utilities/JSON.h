3e41d8142f3956e245e6918853b38cbf02e5590e
Moiz Merchant <moiz@tiktok.com>
2012-01-02 14:03:16 -0800

    Added facebook ios sdk.

---
+++ Classes/Utilities/JSON.h
@@ -0,0 +1,14 @@
+//
+//  JSON.h
+//  TikTok
+//
+//  Created by Moiz Merchant on 1/2/12.
+//  Copyright 2011 TikTok. All rights reserved.
+//
+
+/**
+ * Hack to get Facebook iOS SDK withing with the newer SBJson library.
+ */
+
+#import "SBJson.h"
+#define SBJSON SBJsonParser
