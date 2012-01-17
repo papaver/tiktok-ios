e4a5ffeb0808bdcd4821cb67b7a2953fbfc343bb
Moiz Merchant <moiz@tiktok.com>
2012-01-17 10:14:26 -0800

    Updated imports to be more specific.

--- Classes/Categories/MFMailComposeViewController+Handlers.h
+++ Classes/Categories/MFMailComposeViewController+Handlers.h
@@ -11,7 +11,7 @@
 //-----------------------------------------------------------------------------
 
 #import <Foundation/Foundation.h>
-#import <MessageUI/MessageUI.h>
+#import <MessageUI/MFMailComposeViewController.h>
 
 //-----------------------------------------------------------------------------
 // typedef 
